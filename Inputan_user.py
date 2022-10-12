6afb93b2f25a80fb2b782893ab88b1760e94b8b6
Iamndia <115078535+Iamndia@users.noreply.github.com>
2022-10-12 20:56:36 +0800

    Menerima inputan dari user
    Buatlah sebuah program yang menerima inputan dari user. Adapun data yang perlu ditampung adalah nama depan, nama belakang,umur,tinggi,berat badan,khusus untuk tinggi outputnya dalam bentuk meter.

---
+++ Inputan_user.py
@@ -0,0 +1,17 @@
+"""
+Inputan user
+
+"""
+print(10*"~","INPUT","~"*10)
+nama_depan = input("Masukkan Nama depan = ")
+nama_belakang = input("Masukkan Nama belakang = ")
+tinggi = float(input("Masukkan Tinggi = "))
+berat_badan = float(input("Masukkan Berat badan = "))
+umur = int(input("Masukkan umur = "))
+print("")
+print(10*"=","OUTPUT","="*10)
+print("Nama Depan = ",nama_depan)
+print("Nama belakang = ",nama_belakang)
+print("Tinggi = ",tinggi," m")
+print("Berat badan = ",berat_badan," kg")
+print("Umur = ",umur,"Tahun")
