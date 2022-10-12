4285169d5891f4931960c7f82c5ea25569d9e0c0
Iamndia <115078535+Iamndia@users.noreply.github.com>
2022-10-12 20:50:13 +0800

    Variabel dan tipe data
    Buatlah beberapa variabel yang masing-masing menampung nama depan,nama belakang,umur,tinggi(satuan meter),berat badan(satuan kg), ukuran baju(S,M/L),dan status pernikahan. Kemudian berikan nilai/value kepada setiap variabel tentukan tipe datanya dan jelaskan kenapa menggunakan tipe data tersebut .

---
+++ Variabel_tipedata.py
@@ -0,0 +1,34 @@
+"""
+Variabel dan tipe data
+No.1 dan No.2
+
+"""
+first_name = "Nadia"
+last_name = "TI"
+age = 18
+tall = 1.48
+weight = 37.0
+clothes_size = "L"
+marital_status = False 
+
+print(10*"=","TIPE DATA","="*10)
+print("First name = ",type(first_name))
+print("Last name = ",type(last_name))
+print("Age = ",type(age))
+print("Tall = ",type(tall))
+print("Weight = ",type(weight))
+print("Clothes size = ",type(clothes_size))
+print("Marital status = ",type(marital_status))
+print("\n",10*"=","Alasan menggunakan tipe data tersebut","="*10)
+print("1. Pada variabel first_name dan last_name saya menggunakan tipe data string karena disitu kita akan menggunakan data berupa karakter/huruf.\n")
+print("2. Pada variabel age saya menggunakan tipe data integer karena disitu kita akan menggunkan bilangan bulat,walaupun bisa juga menggunakan string tapi saya menganggap bahwa tipe data integer lebih bagus.\n")
+print("3. Pada variabel tall dan weight saya menggunakan tipe data float karena pada variabel float saya menggunakan satuan meter dan di variabel weight saya menggunakan satuan kg.\n")
+print("4. Pada variabel clothes_size saya menggunakan tipe data string karena disitu saya menggunakan data berupa karakter/huruf(sudah ada di soal).\n")
+print("5. Pada variabel marital_status saya menggunakan tipe data boolean karena saya berpikir hanya tipe data itu yang belum saya gunakan.\n")
+print(10*"=","OUTOUT = JAWABAN NO.2 BAGIAN b","="*10)
+print("Nama = ",first_name, last_name)
+print("Umur = ",age)
+print("Tinggi = ",tall)
+print("Berat badan = ",weight)
+print("Ukuran baju = ",clothes_size)
+print("Status pernikahan = ",marital_status)
