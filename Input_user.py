4a7283f1aef187cd8b6746831d209a130f106940
Iamndia <115078535+Iamndia@users.noreply.github.com>
2022-10-12 18:15:16 +0800

    Menginput data
    Buatlah program yang dapat menerima input dari keyboard.Adapun data yang perlu ditampung adalah nama depan,nama belakang, tinggi,berat badan,umur, status pernikahan dan nomor telpon.

---
+++ Input_user.py
@@ -0,0 +1,24 @@
+"""
+Input user
+
+"""
+print(10*"=","INPUT USER","="*10)
+print("")
+nama_depan = input("Masukkan Nama depan Anda = ")
+nama_belakang = input("Masukkan Nama belakang Anda = ")
+tinggi = input("Masukkan tinggi Anda = ")
+berat_badan = input("Masukkan Berat badan Anda = ")
+umur = input("Masukkan umur Anda = ")
+status_pernikahan = input("Masukkan status pernikahan Anda = ")
+no_telp = input("Masukkan Nomor telpon Anda = ")
+print("")
+
+#cetak hasil
+print(9*"=","OUTPUT","="*9)
+print("")
+print("Nama = ",nama_depan,nama_belakang)
+print("Tinggi = ",tinggi)
+print("Berat badan = ",berat_badan)
+print("Umur = ",umur)
+print("Status pernikahan = ",status_pernikahan)
+print("Nomor telpon = ",no_telp)
