81ab01b8513875b91122f582bb58aa962df8b7d6
Iamndia <115078535+Iamndia@users.noreply.github.com>
2022-10-12 18:27:35 +0800

    Aritmatika
    Amrillah ingin membeli komputer sultan seharga 55jt. Gaji pokok amrillah perbulan adalah 5jt. Setiap bulan amrillah harus membayar uang kost sebesar 750rb. Uang yang dapat ditabung selama sebulan maksimal 1,5jt. Sementara amrillah juga harus memenuhi kebutuhan hidupnya sehari-hari. Berapa lama waktu minimal yang dibutuhkan amrillah menabung untuk bisa membeli komputer sultan tersebut.

---
+++ Aritmatika1.py
@@ -0,0 +1,13 @@
+"""
+Aritmatika
+No.1
+
+"""
+harga_komputer = 5500000000
+gaji_per_bulan = 500000000
+uang_kost = 75000000
+tabungan_per_bulan = 150000000
+bulan = 12
+lama_waktu_menabung = harga_komputer / tabungan_per_bulan
+tahun = lama_waktu_menabung / bulan
+print("Jadi,,,\n lama waktu yang dibutuhka untuk menabung adalah \n",lama_waktu_menabung,"bulan","atau setara dengan",tahun," tahun","jika dibulatkan sekitar 3 tahun.")
