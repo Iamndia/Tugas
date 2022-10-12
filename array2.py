d3558f54629644f1ded8e1f41f63ec07f6065f53
Iamndia <115078535+Iamndia@users.noreply.github.com>
2022-10-13 07:42:34 +0800

    Array
    Tampilkan nilai/value/isi index terakhir dari array/arraylist/list/tuple tersebut.

---
+++ array2.py
@@ -0,0 +1,8 @@
+a = []
+for i in range(10):
+	artis = input("Masukkan nama artis = ")
+	a.append(artis)
+print(10*"=","Nama artis","="*10)
+print("Nama artis = ",a)
+end = a.pop()
+print("Element terakhir = ",end)
