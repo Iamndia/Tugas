a7a86bd801a0b86e898b005eb5a33917e1665d73
Iamndia <115078535+Iamndia@users.noreply.github.com>
2022-10-13 07:57:37 +0800

    Array
    Updatelah nilai/value dari index ke-9 array/arraylist/list/tuple tersebut.

---
+++ array4.py
@@ -0,0 +1,8 @@
+a = []
+for i in range(10):
+	artis = input("Masukkan nama artis = ")
+	a.append(artis)
+print(10*"=","Nama artis","="*10)
+print("Nama artis = ",a)
+a[9] = "Nadia"
+print("Setelah diupdate = ",a)
