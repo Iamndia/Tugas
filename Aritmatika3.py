7579ca1d4ddc0fdb2f472e5101e97fb91aca9d63
Iamndia <115078535+Iamndia@users.noreply.github.com>
2022-10-12 18:42:11 +0800

    Aritmatika
    Buatlah program yang dapat menerima inputan dari keyboard dan dapat melakukan operasi aritmatika (penjumlahan, pengurangan, perkalian, pembagian dan modulus) kemudian tampilkan hasil dari operasi aritmatika tersebut.

---
+++ Aritmatika3.py
@@ -0,0 +1,14 @@
+"""
+Aritmatika 
+No.3
+
+"""
+print(10*"=","MY KALKULATOR","="*10)
+nilai_pertama = int(input("Masukkan nilai pertama = "))
+nilai_kedua = int(input("Masukkan nilai kedua = "))
+print(10*"=","OUTPUT","="*10)
+print(nilai_pertama,"+", nilai_kedua,"=",nilai_pertama + nilai_kedua)
+print(nilai_pertama,"-", nilai_kedua,"=",nilai_pertama - nilai_kedua)
+print(nilai_pertama,"*",nilai_kedua,"=",nilai_pertama * nilai_kedua)
+print(nilai_pertama,"/",nilai_kedua,"=",nilai_pertama / nilai_kedua)
+print(nilai_pertama,"%",nilai_kedua,"=",nilai_pertama % nilai_kedua)
