0c617ab1049645d32e58c02be8c2688232973606
Iamndia <115078535+Iamndia@users.noreply.github.com>
2022-10-12 20:25:42 +0800

    Perulangan
    Buatlah program dengan menggunkan perulangan for / while yang menampilkan bilangan genap antara 1 sampai 100 yang tidak habis dibagi 3.

---
+++ loop1.py
@@ -0,0 +1,9 @@
+print(10*"=","Perulangan","="*10)
+print("")
+print("Bilangan genap yang tidak habis dibagi 3")
+for i in range(1,100+1):
+	if i % 2 == 0 and i % 3 == 1:
+		print(i)
+	else:
+	  print(end =' ')
+	  
