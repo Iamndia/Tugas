07a51307b2af808f73a4cf39c11e022b61114622
Iamndia <115078535+Iamndia@users.noreply.github.com>
2022-10-12 20:36:00 +0800

    Menyelesaikan Program dari flowchart
    ,,,

---
+++ Flowchart.py
@@ -0,0 +1,12 @@
+"""
+Flowchart
+
+"""
+print(10*"=","MENYELESAIKAN KASUS FLOWCHART","="*10)
+x = int(input("Masukkan nilai x = "))
+y = int(input("Masukkan nilai y = "))
+p = x + y
+if(p >= 4):
+	print(x * y)
+else:
+	print(x / y)
